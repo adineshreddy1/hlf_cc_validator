8ed49feb0820286b3e068f701d2cd48d2161ee89
adineshreddy1 <Adineshreddy1@gmail.com>
2019-09-20 15:21:31 +0530

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,10 +1,1 @@
-xterm -e docker exec -it chaincode bash 
-sleep 300
-
-cd /opt/gopath/src/chaincodedev
-
-go build -o cc
-
-CORE_PEER_ADDRESS=peer:7052
-CORE_CHAINCODE_ID_NAME=mycc:0 ./
-go build -o cc
+docker exec -it chaincode bash && go build -o chaincode
